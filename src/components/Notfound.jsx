c018a0a804f28db21950f57ea8376e5806e0d7c7
Shivam Dewangan <110464857+shivamdevs@users.noreply.github.com>
2022-12-12 18:22:03 +0530

    Code update 2.a.b
    Fixed Not-found image issues.

--- src/components/Notfound.jsx
+++ src/components/Notfound.jsx
@@ -9,17 +9,19 @@ function Notfound() {
     return (
         <div className={css.notfound}>
             <BackHeader label="Page not found" />
-            <div className={css.text}>
-                This page is not available right now.<br />
-                Either <strong>go back</strong> or continue to <strong>Home</strong> page.
-            </div>
-            <div className={css.flex}>
-                <NavButton className={css.button} to={-1}>Go back</NavButton>
-                <NavButton className={`${css.button} ${css.filled}`} to="/">Continue to Home</NavButton>
+            <div className={css.wrap}>
+                <div className={css.text}>
+                    This page is not available right now.<br />
+                    Either <strong>go back</strong> or continue to <strong>Home</strong> page.
+                </div>
+                <div className={css.flex}>
+                    <NavButton className={css.button} to={-1}>Go back</NavButton>
+                    <NavButton className={`${css.button} ${css.filled}`} to="/">Continue to Home</NavButton>
+                </div>
             </div>
             {undraw === 0 && <img className={css.image} src="/assets/images/notfound/undraw-404.svg" alt="" />}
             {undraw === 1 && <img className={css.image} src="/assets/images/notfound/undraw-canvas.svg" alt="" />}
-            {undraw === 2 && <img className={css.image} src="/assets/images/notfound/undraw-tree.svg" alt="" />}
+            {undraw === 2 && <img className={css.image} style={{borderTop: "1px solid #ddd"}} src="/assets/images/notfound/undraw-tree.svg" alt="" />}
         </div>
     );
 };

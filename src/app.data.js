5f2b28a8286f1fc0d89c9b217b49540a62fd68ff
Shivam Dewangan <110464857+shivamdevs@users.noreply.github.com>
2023-01-24 16:24:40 +0530

    Version update 1.2.2
    Updated accounts component.

--- src/app.data.js
+++ src/app.data.js
@@ -1,8 +1,8 @@
 const app = {
     name: 'Tasks',
-    version: '1.2.1',
+    version: '1.2.2',
     copyright: <a href="//myoasis.tech" target="_blank" rel="noreferrer">© My Oasis Tech</a>,
-    updated: 'January 17, 2023 02:00 AM',
+    updated: 'January 24, 2023 04:00 PM',
     bucket: "tasks:",
     pathname: "//myoasis.tech",
     parent: "Oasis Tech",
